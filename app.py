8145717572cac08beaa9179617196df8c84b9748
chamodsathsara666-sys <chamodsathsara666@gmail.com>
2026-06-16 15:33:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -50,8 +50,8 @@ st.markdown(f"""
     .nezuko-float {{
         position: fixed;
         top: 200px;
-        right: center;
-        left: center;
+        right: 100px;
+        left: 100px;
         width: 150px;
         height: 150px;
         /* රවුම වෙනුවට හතරැස් හැඩය සඳහා */
