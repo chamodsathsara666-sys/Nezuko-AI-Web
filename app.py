b97443765f3ea68a385c33f3b26d881779a3e17d
chamodsathsara666-sys <chamodsathsara666@gmail.com>
2026-06-16 15:25:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import base64
 api_key = st.secrets["GROQ_API_KEY"]
 client = Groq(api_key=api_key)
 
-st.title("🌸 Nezuko AI 🌸")
+st.title("  🌸 Nezuko AI 🌸")
 
 # පින්තූරය base64 කරන්න හෙල්පර් ෆන්ක්ෂන් එකක්
 def get_image_base64(path):
